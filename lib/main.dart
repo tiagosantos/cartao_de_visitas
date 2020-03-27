b20861c90636fcea8a93e6c0d1d38b21def83173
Carlos Tiago <ontiago@gmail.com>
2020-03-27 17:52:45 -0300

    create card

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,87 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/me.jpg'),
+              ),
+              Text(
+                'Carlos Tiago',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'SOFTWARE ENGINNER',
+                style: TextStyle(
+                    color: Colors.blueAccent[100],
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Raleway'),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.blueAccent.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blueAccent,
+                  ),
+                  title: Text(
+                    '+55 99 99999-9999',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blueAccent,
+                        fontFamily: 'Raleway'),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blueAccent,
+                  ),
+                  title: Text(
+                    'ontiago@gmail.com',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blueAccent,
+                        fontFamily: 'Raleway'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
